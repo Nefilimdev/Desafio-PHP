c2b70fa2d29d4989c4f0d474e9d64b6d65cc2680
Caio Alves <caiojuodzevicius@gmail.com>
2019-11-03 00:52:50 -0300

    Páginas produto/produto.json/var criadas e finalizadas, pasta img criada, página index atualizada.

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+    function cadastrarProduto($nomeProduto,$categoriaProduto,$descProduto,$qtdProduto,$precoProduto,$imgProduto){
+        $nomeArquivo = "produto.json";
+
+        if(file_exists($nomeArquivo)){
+            $arquivo = file_get_contents($nomeArquivo);
+            $produtos = json_decode($arquivo,true);
+            $produtos[] = ["nome"=>$nomeProduto,"categoria"=>$categoriaProduto,"desc"=>$descProduto,"qtd"=>$qtdProduto,"preco"=>$precoProduto,"img"=>$imgProduto];
+            $json = json_encode($produtos);
+            $deuCerto = file_put_contents($nomeArquivo,$json);
+
+        }else{
+            $produtos = [];
+            $produtos[] = ["nome"=>$nomeProduto,"categoria"=>$categoriaProduto,"desc"=>$descProduto,"qtd"=>$qtdProduto,"preco"=>$precoProduto,"img"=>$imgProduto];
+            $json = json_encode($produtos);
+            $deuCerto = file_put_contents($nomeArquivo,$json);
+        }
+    }
+
+    if($_POST){  
+        $nomeImg = $_FILES["imgProduto"]["name"];
+        $locasTmp = $_FILES["imgProduto"]["tmp_name"];
+        $dataAtual = date("d-m-y");
+        $caminhoSalvo = "img/".$dataAtual.$nomeImg;
+
+        $deuCerto = move_uploaded_file($locasTmp,$caminhoSalvo);
+        
+        echo cadastrarProduto($_POST["nomeProduto"],$_POST["categoriaProduto"],$_POST["descProduto"],$_POST["qtdProduto"],$_POST["precoProduto"],$caminhoSalvo);
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,46 +40,99 @@
     <title>Desafio PHP</title>
 </head>
 <body>
-    <?php include_once("config/val.php") ?>
 
-    <main class="container">
+    <?php include_once("var.php"); ?>
 
-        <div class="row">
+        <div class="container">
+            <div class="row p-4">
+                <div class="col-7">
 
-            <div class="col-6"> 
-                <h1>Cadastro de Produtos</h1>   
-                <form action="index.php" method="post">
-                    <div class="form-group">
-                        <div>
-                            <input class="form-control" type="text" name="nomeProduto" id="" placeholder="Nome do produto">
+                    <h1 class="pb-3">Todos os produtos</h1>
+                    
+                    <?php if(isset($produtos) && $produtos != []) {?>
+
+                        <table class="table">
+
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Categoria</th>
+                                    <th scope="col">Preço</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php foreach($produtos as $produto){ ?>
+
+                                    <tr>
+                                        <td><a href="produto.php?nome=<?php echo $produto["nome"]; ?>"><?php echo $produto["nome"]; ?></a></td>
+                                        <td><?php echo $produto["categoria"]; ?></td>
+                                        <td><?php echo "R$ ".$produto["preco"]; ?></td>
+                                    </tr>
+                                    
+                                <?php } ?>
+                            </tbody>
+
+                        </table>
+
+                    <?php }else{ 
+                        echo "<h3>Não tem produtos cadastrados nessa sessão! :(</h3>";
+                        }
+                    ?>
+
+                </div>
+
+                <div class="col-5 bg-light p-5">
+                    <h5 class="pb-3">Cadastrar produto</h5>
+                    <form action="" method="post" enctype="multipart/form-data">
+
+                        <div class="form-group">
+                            <label for="nomeProduto">Nome</label>
+                            <input type="text" class="form-control" id="nomeProduto" name="nomeProduto">
                         </div>
 
-                        <div>
-                            <input class="form-control" type="text" name="descProduto" id="" placeholder="Desrição do produto">
+                        <div class="form-group">
+                            <label for="categoriaProduto">Categoria</label>
+                                <select class="form-control" id="categoriaProduto" name="categoriaProduto">
+                                    <option selected>Selecione uma categoria</option>
+                                    <option>Vestuário</option>
+                                    <option>Acessórios</option>
+                                    <option>Tecnologia</option>
+                                    <option>Decoração</option>
+                                </select>
                         </div>
 
-                        <div>
-                            <input class="form-control" type="text" name="categoriaProduto" id="" placeholder="Categoria do produto">
+                        <div class="form-group">
+                            <label for="descProduto">Descrição</label>
+                            <textarea class="form-control noresize" id="descProduto" name="descProduto" rows="3"></textarea>
                         </div>
 
-                        <div>
-                            <input class="form-control" type="number" name="quantidadeProduto" id="" placeholder="Quantidade do produto">
+                        <div class="form-group">
+                            <label for="qtdProduto">Quantidade</label>
+                            <input type="number" class="form-control" id="qtdProduto" name="qtdProduto">
                         </div>
 
-                        <div>
-                            <input class="form-control" type="number" name="precoProduto" id="" placeholder="Preço do produto">
+                        <div class="form-group">
+                            <label for="precoProduto">Preço</label>
+                            <input type="number" class="form-control" id="precoProduto" name="precoProduto">
                         </div>
 
-                        <div>
-                            <input class="form-control" type="file" name="fotoProduto" id="" placeholder="Foto do produto">
+                        <div class="form-group">
+                            <label for="imgProduto">Foto do produto</label>
+                            <input type="file" class="form-control-file" id="imgProduto" name="imgProduto">
                         </div>
 
-                        <button class="btn btn-success" type="submit">Cadastrar Produto</button>
-                    </div>
-                </form>
-            </div>    
+                        <div class="text-right">
+                            <button class="btn btn-success">Enviar</button>
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
         </div>
-
     
 </body>
 </html>
